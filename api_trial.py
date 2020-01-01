07f0b868bb8093e952d622d21f3a6f5e0ee1516d
Pablo Bee Olmedo <pbeeolmedo@gmail.com>
2020-01-01 22:17:32 +1100

    finally got a 200 repsonse from api

--- api_trial.py
+++ api_trial.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import requests
 import json
-from .contrasena import api_key
+from contrasena import api_key
 
-print(api_key)
-
-response = requests.get("https://api.football-data.org/v2/competitions/PL/matches")
 headers = {'X-Auth-Token': api_key}
+
+response = requests.get("https://api.football-data.org/v2/competitions/PL/matches",headers=headers)
+
 print(response.status_code)
