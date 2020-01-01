1b1f15aaa6012256989954a7b3658e0ce84fa070
Pablo Bee Olmedo <pbeeolmedo@gmail.com>
2020-01-01 22:15:04 +1100

    minor changes

--- api_trial.py
+++ api_trial.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 import requests
 import json
+from .contrasena import api_key
+
+print(api_key)
+
+response = requests.get("https://api.football-data.org/v2/competitions/PL/matches")
+headers = {'X-Auth-Token': api_key}
+print(response.status_code)
